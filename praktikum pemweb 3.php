c2bb6f8d2dc7f65ce5311861ed6a88a87239c341
zai15 <92990206+zai15@users.noreply.github.com>
2022-03-22 14:51:59 +0700

    Add files via upload

---
+++ praktikum pemweb 3.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>form nilai</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"> 
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+    <div class="alert alert-success">Sistem penilaian</div>
+    <h1>Form Nilai Siswa</h1>
+    <form method="POST" action="form_siswa.php">
+        <div class="container">
+            <div class="form-group row">
+                <label for="nama" class="col-4 col-form-label">Nama Lengkap</label> 
+                <div class="col-8">
+                    <input id="nama" name="nama" placeholder="Masukan nama lengkap" type="text" class="form-control">
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="matkul" class="col-4 col-form-label">Mata Kuliah</label> 
+                <div class="col-8">
+                    <select id="matkul" name="matkul" class="custom-select">
+                        <option value="DDP">Dasar-Dasar Pemograman</option>
+                        <option value="Basdat">Basis data 1</option>
+                        <option value="web1">Pemograman web 1</option>
+                    </select>
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="nilai_uts" class="col-4 col-form-label">Nilai UTS</label> 
+                <div class="col-8">
+                    <input id="nilai_uts" name="nilai_uts" placeholder="Masukan nilai UTS" type="text" class="form-control">
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="nilai_uas" class="col-4 col-form-label">Nilai UAS</label> 
+                <div class="col-8">
+                    <input id="nilai_uas" name="nilai_uas" placeholder="Masukan nilai UAS" type="text" class="form-control">
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="nilai_tugas" class="col-4 col-form-label">Nilai Tugas Praktikum</label> 
+                <div class="col-8">
+                    <input id="nilai_tugas" name="nilai_tugas" placeholder="Masukan nilai tugas" type="text" class="form-control">
+                </div>
+            </div> 
+            <div class="form-group row">
+                <div class="offset-4 col-8">
+                    <button name="proses" type="submit" class="btn btn-primary" value="Simpan">Submit</button>
+                </div>
+            </div>        
+        </div>    
+    </form>
+    <div class="alert alert-success" style="top:11rem;">Build by Zaidan ahmadillah &#169; 2022</div>
+</body>
+</html>
